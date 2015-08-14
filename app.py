fd61c7d4a88b8ad2cb3ff653320b4dfe86a7dd18
Crystal <crystal@uq.edu.au>
2015-08-14 08:19:03 +0000

    teaching git

--- app.py
+++ app.py
@@ -2,6 +2,7 @@ from KV import KV
 from flask import Flask
 from flask import render_template, request, redirect, send_file
 import io
+import json
 
 app = Flask(__name__)
 
@@ -9,10 +10,14 @@ app.secret_key = ''
 
 @app.route("/")
 def frontPage():
-    #addr = '172.23.84.20';
-    #kv = new KV(addr);
-    #return redirect(url_for('project'), code=302)
-    return render_template('index.html')
+    user_info = json.loads(request.headers.get('X-KVD-Payload'))
+    return render_template('index.html', name=user_info['name'])
+
+@app.route("/crystal")
+def ME():
+    user_info = json.loads(request.headers.get('X-KVD-Payload'))
+    return render_template('crystal.html', name=user_info['name'],
+	    THINGS = "RABBITS")
 
 @app.route("/profile")
 def profilePage():
@@ -24,11 +29,12 @@ def profilePage():
 
 @app.route("/course")
 def courseDefault():
+    user_info = json.loads(request.headers.get('X-KVD-Payload'))
     lipsum = "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum."
     courselist = [['Security', lipsum, 2, 5], 
 	    ['All About Cats', lipsum, 1, 5],
 	    ['How To Train Your Dragon', lipsum, 5, 5]]
-    return render_template('course.html', courses = courselist)
+    return render_template('course.html', courses = courselist, name=user_info['name'])
 
 @app.route("/course/<courseid>", methods=['GET', 'POST'])
 def coursePage():
@@ -38,7 +44,6 @@ def coursePage():
 def login():
     pass
 
-
 # DO NOT TOUCH THIS SECTION DO NOT DO IT I WILL KNOW AND I WILL SMACK YOU
 if __name__ == "__main__":
-	app.run(host='0.0.0.0', port=80, debug=True)
+	app.run(host='0.0.0.0', port=8000, debug=True)

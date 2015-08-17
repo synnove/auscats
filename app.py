d7974f9e364e0b0b2edbbff4289c00338ba787b4
Crystal <crystal@uq.edu.au>
2015-08-17 14:36:54 +0000

    added placeholder data for courses & a query; now displays course info from database (incomplete)

--- app.py
+++ app.py
@@ -3,6 +3,7 @@ from flask import Flask
 from flask import render_template, request, redirect, send_file
 import io
 import json
+import queries as db
 
 app = Flask(__name__)
 
@@ -13,14 +14,12 @@ def frontPage():
     user_info = json.loads(request.headers.get('X-KVD-Payload'))
     return render_template('index.html', name=user_info['name'])
 
-@app.route("/course")
+@app.route("/courses")
 def courseDefault():
     user_info = json.loads(request.headers.get('X-KVD-Payload'))
-    lipsum = "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum."
-    courselist = [['Security', lipsum, 2, 5], 
-	    ['All About Cats', lipsum, 1, 5],
-	    ['How To Train Your Dragon', lipsum, 5, 5]]
-    return render_template('course.html', courses = courselist, name=user_info['name'])
+    courses = db.get_course_info()
+    return render_template('course.html', name=user_info['name'],
+	    courses = courses)
 
 @app.route("/course/<courseid>", methods=['GET', 'POST'])
 def coursePage(courseid):
--- queries.py
+++ queries.py
@@ -3,6 +3,15 @@ import hashlib
 
 db = None
 
+def get_course_info():
+    courses = list()
+    db = do_mysql_connect()
+    cur = db.cursor()
+    cur.execute("SELECT * FROM COURSES")
+    for row in cur.fetchall():
+	courses.append(row)
+    return courses
+
 def read_mysql_password():
     f = open('mysql.passwd', 'r')
     passwd = f.read()
@@ -12,7 +21,7 @@ def read_mysql_password():
 def do_mysql_connect():
     global db
     if db == None:
-    db = MySQLdb.connect(db='dbname', host='localhost', port=3306, 
-	    user='root', passwd=read_mysql_password(), 
-	    cursorclass=MySQLdb.cursors.DictCursor)
+	db = MySQLdb.connect(db='auscats', host='localhost', port=3306, 
+		user='root', passwd=read_mysql_password(), 
+		cursorclass=MySQLdb.cursors.DictCursor)
     return db

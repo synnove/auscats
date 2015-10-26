c8713e96e9db067088c2084b44fb531119ff2c95
Nizar <nizarberjawi12@gmail.com>
2015-10-26 08:28:03 +0000

    Trying to get Manage Administrators to work

--- static/js/admin_manage.js
+++ static/js/admin_manage.js
@@ -34,6 +34,42 @@ $('#admins').bind('submit', function(e) {
     }
 });
 
+$(document).foundation('alert', 'reflow');
+$('#admins2').bind('submit', function(e) {
+    e.preventDefault();
+    $url = "/admin_mod/action=add";
+    if ($("#large-2 columns panel listing text-left span").prop("title").indexOf("uq") == 0) {
+    $url += "&user=";
+    $url += $("#large-2 columns panel listing text-left span").prop("title");
+    if ($('#write_access2').prop('checked')) {
+        $url += "&write=true";
+    }
+    if ($('#read_access2').prop('checked')) {
+        $url += "&read=true";
+    }
+    if ($("#admins2 input:checkbox:checked").length >= 1) {
+        console.log("everything ok");
+        console.log($url);
+        window.location = $url;
+    } else {
+        console.log("no perms");
+        $("#admin_err").html(function() {
+        return "<div data-alert class='alert-box warning radius text-center'>" +
+            "<b>No permissions specified.</b>" + "<a href='#' class='close'>&times;</a>" + "</div>";
+        });
+        $(document).foundation();
+        $(document).foundation('alert', 'reflow');
+    }
+    } else {
+    $("#admin_err").html(function() {
+        return "<div data-alert class='alert-box warning radius text-center'>" +
+        "<b>Please enter a valid user.</b>" + "<a href='#' class='close'>&times;</a>" + "</div>";
+        });
+    $(document).foundation();
+    $(document).foundation('alert', 'reflow');
+    }
+});
+
 $(document).on('close.fndtn.alert', function(event) {
     $(document).foundation();
 });
